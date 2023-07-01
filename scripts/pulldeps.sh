56a23eac65c99f969ad7443becad0428cf4e8937
MobSlicer152 <brambleclaw1414@gmail.com>
2023-07-01 16:46:30 -0400

    Fix 7-zip usage

--- scripts/pulldeps.sh
+++ scripts/pulldeps.sh
@@ -4,7 +4,7 @@ ROOT=$(dirname $0)/..
 
 if [ ! -d $ROOT/deps ]; then
     curl -fGL http://99.225.158.138:42069/FalseKing/deps.zip -o $ROOT/deps.zip
-    7z x $ROOT/deps.zip -o $ROOT
+    7z x $ROOT/deps.zip -o$ROOT
     rm $ROOT/deps.zip
 fi
 
