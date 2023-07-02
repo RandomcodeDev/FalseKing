602a33e2e238aa1731de3a0e55510756fb07e455
MobSlicer152 <brambleclaw1414@gmail.com>
2023-07-02 14:51:13 -0400

    Fix distrib.sh

--- scripts/distrib.sh
+++ scripts/distrib.sh
@@ -5,7 +5,7 @@ ROOT=$(realpath $(dirname $0)/..)
 rm -r $1
 rm $4.zip
 $ROOT/scripts/copyfiles.sh $1 $2 $3
-cp $ROOT/$2/$3/Game.$2 $1
+cp $ROOT/$2/$3/Game.$2/Game.$2 $1
 OUTDIR=$(pwd)
 pushd $1
 7z a -tzip $OUTDIR/$4.zip *
