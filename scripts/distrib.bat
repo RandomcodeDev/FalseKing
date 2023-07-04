0f30f7bc3fdb3f7f61899356d50c1cbbbcccc36a
MobSlicer152 <39959519+MobSlicer152@users.noreply.github.com>
2023-07-04 15:16:31 -0400

    Fix distrib scripts and build.yml

--- scripts/distrib.bat
+++ scripts/distrib.bat
@@ -2,17 +2,17 @@
 
 setlocal enabledelayedexpansion enableextensions
 
-rmdir /s/q %1
-del %4.zip
+rmdir /s/q "%1"
+del "%4.zip"
 
-call %~dp0copyfiles %1 %2 %3
+call "%~dp0copyfiles" "%1" "%2" "%3"
 
-copy %~dp0..\build\winxp\%2\%3\Game.%2.exe %1
-copy %~dp0..\build\winxp\%2\%3\Game.%2.pdb %1
+copy "%~dp0..\build\winxp\%2\%3\Game.%2.exe" "%1"
+copy "%~dp0..\build\winxp\%2\%3\Game.%2.pdb" "%1"
 
-set OUTDIR=%CD%
-pushd %1
-7z a -tzip %OUTDIR%\%4.zip *
+set "OUTDIR=%CD%"
+pushd "%1"
+7z a -tzip "%OUTDIR%\%4.zip" *
 popd
-rmdir /s/q %1
+rmdir /s/q "%1"
 
--- scripts/distrib.sh
+++ scripts/distrib.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env sh
 
-ROOT=$(realpath $(dirname $0)/..)
+ROOT="$(realpath $(dirname $0)/..)"
 
-rm -r $1
-rm $4.zip
-$ROOT/scripts/copyfiles.sh $1 $2 $3
-cp $ROOT/build/unix/$2/$3/Game.$2 $1
+rm -r "$1"
+rm "$4.zip"
+mkdir -p "$1"
+$ROOT/scripts/copyfiles.sh "$1" "$2" "$3"
+cp "$ROOT/build/unix/$2/$3/Game.$2" "$1"
 OUTDIR=$(pwd)
-pushd $1
-7z a -tzip $OUTDIR/$4.zip *
+pushd "$1"
+7z a -tzip "$OUTDIR/$4.zip" *
 popd
-rm -r $1
+rm -r "$1"
