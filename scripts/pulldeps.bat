dc012425a8130f76c2849cccbeb69171cc7be423
MobSlicer152 <brambleclaw1414@gmail.com>
2023-07-21 21:23:59 -0400

    Change IP address to domain name

--- scripts/pulldeps.bat
+++ scripts/pulldeps.bat
@@ -1,6 +1,6 @@
 @echo off
 
 if not exist "%~dp0..\deps" (
-    git clone "http://99.225.158.138:3000/mobslicer152/FalseKing-deps-%1" "%~dp0..\deps-%1"
+    git clone "https://git.randomcode.dev/mobslicer152/FalseKing-deps-%1" "%~dp0..\deps-%1"
 )
 
