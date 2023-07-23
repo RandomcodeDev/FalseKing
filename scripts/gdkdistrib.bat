79f6721d6eafef2bae250547d034667cf469f785
MobSlicer152 <brambleclaw1414@gmail.com>
2023-07-22 22:31:43 -0400

    Improve GDK stuff

---
+++ scripts/gdkdistrib.bat
@@ -0,0 +1,36 @@
+@echo off
+
+setlocal enabledelayedexpansion enableextensions
+
+if "%2" == "Gaming.Scarlett.x64" (
+	set varstype=GamingXboxVS2022
+	set extraparams=
+) else (
+	set varstype=GamingDesktopVS2022
+	set extraparams=/pc
+)
+
+set "ORIGCD=%CD%"
+call "%GameDK%Command Prompts\GamingDesktopVars.cmd" %varstype%
+cd /d %ORIGCD%
+
+rmdir /s/q "%1"
+rmdir /s/q "%4"
+
+call "%~dp0copyfiles" "%1" "%2" "%3"
+
+copy "%~dp0..\build\windows\%2\%3\Game.%2.exe" "%1"
+copy "%~dp0..\build\windows\%2\%3\Game.%2.pdb" "%1"
+
+copy "%~dp0..\build\windows\%2\%3\MicrosoftGame.Config" "%1"
+copy "%~dp0..\build\windows\%2\%3\"*.dll "%1"
+xcopy /e/i/y "%~dp0..\build\windows\GdkAssets" "%1\GdkAssets"
+xcopy /e/i/y "%~dp0..\deps-public\licenses" "%1\licenses"
+copy "%~dp0..\LICENSE.txt" "%1\licenses\falseking.txt"
+
+mkdir "%4"
+makepkg genmap /f "layout.xml" /d "%1"
+makepkg pack /f "layout.xml" /lt /d "%1" /nogameos %extraparams% /pd "%4"
+copy "%4\*.msixvc" "%4.msixvc"
+del "layout.xml"
+rmdir /s/q "%1"
