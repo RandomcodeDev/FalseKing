beef57589419ec78eb93221baf59e91af0fbbe8c
MobSlicer152 <brambleclaw1414@gmail.com>
2023-07-30 01:41:37 -0400

    Move chmod out of pulldeps

--- scripts/pulldeps.sh
+++ scripts/pulldeps.sh
@@ -4,13 +4,12 @@ ROOT="$(realpath $(dirname $0)/..)"
 
 if [ $# -lt 1 ]; then exit; fi
 
+ORIG=$CD
+
 if [ ! -d "$ROOT/deps-$1" ]; then
     git clone --depth=1 "https://git.randomcode.dev/mobslicer152/FalseKing-deps-$1" "$ROOT/deps-$1"
 else
-    pushd "$ROOT/deps-$1"
+    cd "$ROOT/deps-$1"
     git pull
-    popd
+    cd $ORIG
 fi
-
-chmod a+x "$ROOT/deps-$1/bin/*"
-
