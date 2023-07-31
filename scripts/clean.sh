d6595bb58591d50fa56a41d06c944eaca7c9fc8f
MobSlicer152 <brambleclaw1414@gmail.com>
2023-07-30 23:15:24 -0400

    Make all scripts use Bash

--- scripts/clean.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-ROOT=$(realpath $(dirname $0)/..)
-rm -rf $ROOT/build/unix/{ARM64,x64,Debug,Release}
--- scripts/copyfiles.sh
+++ scripts/copyfiles.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 ROOT="$(realpath $(dirname $0)/..)"
 
