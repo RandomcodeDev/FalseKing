0b646726609d56eeeb2a56ac98aa635147e2a2db
MobSlicer152 <brambleclaw1414@gmail.com>
2023-10-16 08:13:38 -0400

    Start making VPK tool

--- tools/src/bin/vpktool.rs
+++ tools/src/bin/vpktool.rs
@@ -1,3 +1,27 @@
-//use common::vpk;
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+use common::vpk;
+
+#[derive(Subcommand)]
+enum Commands {
+    Create{input_dir: PathBuf, output_vpk: PathBuf},
+    Extract{input_vpk: PathBuf, output_dir: PathBuf},
+    List{input_vpk: PathBuf, check_hashes: bool}
+}
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = Some("Manipulates Valve Pack Files"))]
+#[command(propagate_version = true)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+
+    #[arg(short, long, default_value_t = false)]
+    verbose: bool,
+
+    #[arg(short, long, default_value_t = false)]
+    debug: bool,
+}
 
 fn main() {}
